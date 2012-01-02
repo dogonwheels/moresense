a740d85060cab0cd48e925a9961194a39b44bbfa
Dom Crayford <domcrayford@gmail.com>
2012-01-02 19:55:49 +0000

    Added household and person models.

--- moresense/spendings/models.py
+++ moresense/spendings/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Household(models.Model):
+    name = models.CharField(max_length=50)
+
+class Person(models.Model):
+    household = models.ForeignKey(Household)
+    name = models.CharField(max_length=50)
+    identifier = models.CharField(max_length=50)
+
 class Spending(models.Model):
+    person = models.ForeignKey(Person)
     amount = models.DecimalField(decimal_places=2, max_digits=8)
     description = models.TextField()
