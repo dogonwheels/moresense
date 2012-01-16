a2cd390652dd121c98b97e0d366f61e668cb8767
Dom Crayford <domcrayford@gmail.com>
2012-01-16 17:15:38 +0000

    Added list of spendings.

--- moresense/spendings/views.py
+++ moresense/spendings/views.py
@@ -42,7 +42,7 @@ def add_spending(request, person_identifier):
 
 def spendings_list(request, person_identifier):
     person = get_object_or_404(Person, identifier=person_identifier)
-    spendings = Spending.objects.filter(person__household=person.household)
+    spendings = Spending.objects.filter(person__household=person.household).order_by("-when")
     return render_to_response("spendings/spending_list.html", {
         'person': person,
         'spendings': spendings
