cb87dc02032a5ee7a8aee427374e9115874689d1
Dom Crayford <domcrayford@gmail.com>
2012-01-18 21:16:08 +0000

    Styled list view to look more like a statement

--- moresense/spendings/views.py
+++ moresense/spendings/views.py
@@ -42,7 +42,7 @@ def add_spending(request, person_identifier):
 
 def spendings_list(request, person_identifier):
     person = get_object_or_404(Person, identifier=person_identifier)
-    spendings = Spending.objects.filter(person__household=person.household).order_by("-when")[:10]
+    spendings = Spending.objects.filter(person__household=person.household).order_by("-when")[:25]
     return render_to_response("spendings/spending_list.html", {
         'person': person,
         'spendings': spendings
